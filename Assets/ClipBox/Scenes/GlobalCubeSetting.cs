81040a8022745cd7b0382df61cbbc1dd87943bd7
wo_fpc <wo_fpc@outlook.com>
2017-08-11 18:21:00 +0800

    clip shader with global box limit, bugs when rotation box

---
+++ Assets/ClipBox/Scenes/GlobalCubeSetting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCubeSetting : MonoBehaviour {
+
+    public Vector3 boxSize;
+
+	// Use this for initialization
+	void Start () {
+        Shader.SetGlobalVector("_BoxSize", new Vector4(boxSize.x, boxSize.y, boxSize.z, 1));
+        Shader.SetGlobalVector("_BoxRotation", new Vector4(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z, 1));
+        Shader.SetGlobalVector("_Origin", new Vector4(transform.position.x, transform.position.y, transform.position.z, 1));
+	}
+
+    private void Update()
+    {
+        Shader.SetGlobalVector("_BoxSize", new Vector4(boxSize.x, boxSize.y, boxSize.z, 1));
+        Shader.SetGlobalVector("_BoxRotation", new Vector4(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z, 1));
+        Shader.SetGlobalVector("_Origin", new Vector4(transform.position.x, transform.position.y, transform.position.z, 1));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale); 
+        Gizmos.DrawWireCube(Vector3.zero, boxSize);
+    }
+}
