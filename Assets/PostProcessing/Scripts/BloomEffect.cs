2db87400db68cca389a49ff9b45fa9202353b9f6
wo_fpc <wo_fpc@outlook.com>
2018-04-11 17:55:00 +0800

    boxsampler

--- Assets/PostProcessing/Scripts/BloomEffect.cs
+++ Assets/PostProcessing/Scripts/BloomEffect.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 
 namespace PostProcessing
 {
@@ -7,16 +8,27 @@ namespace PostProcessing
 	{
 		[Range(1, 16)]
 		public int iterations = 1;
+		public Shader bloomShader;
 
+		private const int BoxDownPass = 0;
+		private const int BoxUpPass = 1;
+
+		[NonSerialized]
+		private Material bloom;
 		private RenderTexture[] textures = new RenderTexture[16];
 
 		void OnRenderImage(RenderTexture src, RenderTexture dest)
 		{
+			if (bloom == null)
+			{
+				bloom = new Material(bloomShader);
+				bloom.hideFlags = HideFlags.HideAndDontSave;
+			}
 			var width = src.width;
 			var height = src.height;
 			var format = src.format;
 			RenderTexture currentDest = textures[0] = RenderTexture.GetTemporary(width, height, 0, format);
-			Graphics.Blit(src, currentDest);
+			Graphics.Blit(src, currentDest, bloom, BoxDownPass);
 			RenderTexture currentSrc = currentDest;
 
 			int i = 1;
@@ -29,7 +41,7 @@ namespace PostProcessing
 				}
 
 				currentDest = textures[i] = RenderTexture.GetTemporary(width, height, 0, format);
-				Graphics.Blit(currentSrc, currentDest);
+				Graphics.Blit(currentSrc, currentDest, bloom, BoxDownPass);
 				currentSrc = currentDest;
 			}
 
@@ -38,12 +50,12 @@ namespace PostProcessing
 			{
 				currentDest = textures[i];
 				textures[i] = null;
-				Graphics.Blit(currentSrc, currentDest);
+				Graphics.Blit(currentSrc, currentDest, bloom, BoxUpPass);
 				RenderTexture.ReleaseTemporary(currentSrc);
 				currentSrc = currentDest;
 			}
 
-			Graphics.Blit(currentSrc, dest);
+			Graphics.Blit(currentSrc, dest, bloom, BoxUpPass);
 			RenderTexture.ReleaseTemporary(currentSrc);
 		}
 	}
