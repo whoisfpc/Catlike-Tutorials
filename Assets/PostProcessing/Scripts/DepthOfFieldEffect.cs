e7da1a2a7f227cbb56fa1aaeef131100716ce75a
wo_fpc <wo_fpc@outlook.com>
2018-04-17 14:17:31 +0800

    depth of field, Circle of Confusion

---
+++ Assets/PostProcessing/Scripts/DepthOfFieldEffect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace PostProcessing
+{
+	[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+	public class DepthOfFieldEffect : MonoBehaviour
+	{
+		[HideInInspector]
+		public Shader dofShader;
+		[Range(0.1f, 100f)]
+		public float focusDistance = 10f;
+		[Range(0.1f, 10f)]
+		public float focusRange = 3f;
+
+		[NonSerialized]
+		private Material dofMaterial;
+
+		private const int circleOfConfusionPass = 0;
+
+
+		void OnRenderImage (RenderTexture src, RenderTexture dest)
+		{
+			if (dofMaterial == null)
+			{
+				dofMaterial = new Material(dofShader);
+				dofMaterial.hideFlags = HideFlags.HideAndDontSave;
+			}
+			dofMaterial.SetFloat("_FocusDistance", focusDistance);
+			dofMaterial.SetFloat("_FocusRange", focusRange);
+
+			RenderTexture coc = RenderTexture.GetTemporary(src.width, src.height, 0, RenderTextureFormat.RHalf, RenderTextureReadWrite.Linear);
+			Graphics.Blit(src, coc, dofMaterial, circleOfConfusionPass);
+			Graphics.Blit(coc, dest);
+			RenderTexture.ReleaseTemporary(coc);
+		}
+	}
+}
---
+++ Assets/PostProcessing/Shaders/DepthOfField.shader
@@ -0,0 +1,59 @@
+﻿Shader "Hidden/DepthOfField"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+	CGINCLUDE
+		#include "UnityCG.cginc"
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float2 uv : TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float2 uv : TEXCOORD0;
+			float4 vertex : SV_POSITION;
+		};
+
+		v2f vert (appdata v)
+		{
+			v2f o;
+			o.vertex = UnityObjectToClipPos(v.vertex);
+			o.uv = v.uv;
+			return o;
+		}
+		
+		sampler2D _MainTex, _CameraDepthTexture;
+		float4 _MainTex_TexelSize;
+
+		float _FocusDistance, _FocusRange;
+	ENDCG
+
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass // 0 circleOfConfusionPass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			half frag (v2f i) : SV_Target
+			{
+				float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+				depth = LinearEyeDepth(depth);
+				float coc = (depth - _FocusDistance) / _FocusRange;
+				coc = clamp(coc, -1, 1);
+				return coc;
+			}
+			ENDCG
+		}
+	}
+}
